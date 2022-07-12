0454c36d8f78d674fabcf0dab4270a0f2e513aa5
Luiz Carlos Silvestrini <luizsilvestrini15@hotmail.com>
2022-07-12 11:10:02 -0300

    Project created

---
+++ backand/src/main/java/com/devsuperior/dsmeta/ProjetoDaSemanaReactDevSuperiorApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDaSemanaReactDevSuperiorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDaSemanaReactDevSuperiorApplication.class, args);
+	}
+
+}
