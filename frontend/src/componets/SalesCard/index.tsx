e2185d77e1f5090eafe1ddfc80a0cd0e7d1b9014
Luiz Carlos Silvestrini <luizsilvestrini15@hotmail.com>
2022-07-18 15:56:28 -0300

    Sale listing

--- frontend/src/componets/SalesCard/index.tsx
+++ frontend/src/componets/SalesCard/index.tsx
@@ -5,6 +5,8 @@ import './styles.css';
 import NotificationButton from '../NotificationButton';
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { BASE_URL } from "../../utils/request";
+import { Sale } from "../../models/sales";
 function SalesCard() {
 
     const min = new Date(new Date().setDate(new Date().getDate() - 365));
@@ -13,10 +15,12 @@ function SalesCard() {
     const[minDate, setMinDade]=useState(min);
     const[maxDate, setMaxDade]=useState(max);
 
+    const[sales, setSales] = useState<Sale[]>([]);
+
 useEffect(() =>{
-    axios.get("http://localhost:8080/sales")
+    axios.get(`${BASE_URL}/sales`)
     .then(response => {
-        console.log(response.data);
+        setSales(response.data.content);
     })
 }, []);
 
@@ -55,47 +59,27 @@ useEffect(() =>{
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td className="show992">341</td>
-                            <td className="show576">08/07/2022</td>
-                            <td>Anakin</td>
-                            <td className="show992">15</td>
-                            <td className="show992">11</td>
-                            <td>R$ 55300.00</td>
+                        {sales.map(sale => {
+                            return(
+                            <tr key = {sale.id}>
+                            <td className="show992">{sale.id}</td>
+                            <td className="show576">{new Date(sale.date).toLocaleDateString()}</td>
+                            <td>{sale.sellerName}</td>
+                            <td className="show992">{sale.visited}</td>
+                            <td className="show992">{sale.deals}</td>
+                            <td>R$ {sale.amount.toFixed(2)}</td>
                             <td>
                                 <div className="dsmeta-red-btn-container">
-                                    <NotificationButton />                                </div>
-
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="show992">342</td>
-                            <td className="show576">09/07/2022</td>
-                            <td>Anakin</td>
-                            <td className="show992">15</td>
-                            <td className="show992">11</td>
-                            <td>R$ 55300.00</td>
-                            <td>
-                                <div className="dsmeta-red-btn-container">
-                                    <NotificationButton />
+                                    <NotificationButton />                              
                                 </div>
 
                             </td>
                         </tr>
-                        <tr>
-                            <td className="show992">343</td>
-                            <td className="show576">10/07/2022</td>
-                            <td>Anakin</td>
-                            <td className="show992">15</td>
-                            <td className="show992">11</td>
-                            <td>R$ 55300.00</td>
-                            <td>
-                                <div className="dsmeta-red-btn-container">
-                                    <NotificationButton />
-                                </div>
+                            )
 
-                            </td>
-                        </tr>
+                        })}
+                        
+                        
 
                     </tbody>
                 </table>
