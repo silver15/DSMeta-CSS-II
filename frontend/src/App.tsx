cced3a8fae4c1c23067e13573f9a7772b6d9b7c1
Luiz Carlos Silvestrini <luizsilvestrini15@hotmail.com>
2022-07-18 21:27:36 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
 import Header from "./componets/Header";
 import SalesCard from "./componets/SalesCard";
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
     return (
         <>
+        <ToastContainer/>
             <Header />
             <main>
                 <section id="sales">
